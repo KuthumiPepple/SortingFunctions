dffd199511a41862943a1cfa06681196ef76469e
Kuthumi Pepple <kuthumipepple@gmail.com>
2019-02-27 08:14:53 +0000

    Modify insertion_sort.js to return sorted array

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,12 +1,16 @@
 /* A javascript implementation of the insertion sort algorithm */
 function insertionSort(arr){
 	let len = arr.length;
-	let key, i, j = 1;
+	let key, i, j = 1;// refer to freecodecamp.org for more explanation on this implementation
 	while(j < len){
 		key = arr[j];
 		i = j - 1;
 		while(i >= 0 && arr[i] > key){
 			arr[i + 1] = arr[i];
+			i--;
 		}
+		arr[i + 1] = key;
+		j++;
 	}
+	return arr
 }
